e5f6e6d5636dee41f1f38eafcee6d7666f459179
Scott Bolin <sbolin@gmail.com>
2020-10-24 03:24:18 +0700

    Added Core Data Controller

---
+++ MovieDBCheck/Model/Core Data/CoreDataController.swift
@@ -0,0 +1,54 @@
+//
+//  CoreDataController.swift
+//  MovieDBCheck
+//
+//  Created by Scott Bolin on 10/24/20.
+//
+
+import Foundation
+import CoreData
+
+
+class CoreDataController {
+    //MARK: - Create CoreData Stack
+    static let shared = CoreDataController() // singleton
+    init() {} // Change from private to allow subclassing with new init for unit testing
+    
+    lazy var modelName = "MovieModel"
+    lazy var model: NSManagedObjectModel = {
+        let modelURL = Bundle.main.url(forResource: modelName, withExtension: "momd")!
+        return NSManagedObjectModel(contentsOf: modelURL)!
+    }()
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: modelName, managedObjectModel: model)
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error as NSError? {
+                print("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    lazy var managedContext: NSManagedObjectContext = {
+        let context = self.persistentContainer.viewContext
+        context.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+        return context
+    }()
+    
+    lazy var backgroundContext: NSManagedObjectContext = {
+        let context = self.persistentContainer.newBackgroundContext()
+        return context
+    }()
+    
+    //MARK: - SaveContext
+    func saveContext(managedContext: NSManagedObjectContext) {
+        guard managedContext.hasChanges else { return }
+        do {
+            try managedContext.save()
+        } catch let error as NSError {
+            print("Unresolved error \(error), \(error.localizedDescription)")
+        }
+    }
+    
+}
