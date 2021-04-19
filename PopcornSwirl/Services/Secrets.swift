736a950afc019b0ce1fc054abfe67a9e2425ac17
Scott Bolin <sbolin@gmail.com>
2021-04-19 12:25:15 +0700

    still fixing things

---
+++ PopcornSwirl/Services/Secrets.swift
@@ -0,0 +1,12 @@
+//
+//  Secrets.swift
+//  PopcornSwirl
+//
+//  Created by Scott Bolin on 19-Apr-21.
+//
+
+import Foundation
+
+enum Secrets {
+    static let apiKey = "a042fdafc76ac6243a7d5c85b930f1f6"
+}
